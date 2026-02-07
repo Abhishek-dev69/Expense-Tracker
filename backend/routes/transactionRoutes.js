8d87031c3654a0858d986ce58f7ad8cb552ab07f
Abhishek <abhishek12439525@gmail.com>
2026-02-07 17:05:47 +0530

    server changes

---
+++ backend/routes/transactionRoutes.js
@@ -0,0 +1,41 @@
+import express from "express"
+import { protect } from "../middleware/authMiddleware.js"
+import Transaction from "../models/Transaction.js"
+
+const router = express.Router()
+
+// ➕ Add Income
+router.post("/income", protect, async (req, res) => {
+  try {
+    const tx = await Transaction.create({
+      user: req.user.id,
+      title: req.body.title || "Income",
+      amount: Math.abs(req.body.amount),
+      type: "income",
+      category: req.body.category,
+    })
+
+    res.status(201).json(tx)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+// ➖ Add Expense
+router.post("/expense", protect, async (req, res) => {
+  try {
+    const tx = await Transaction.create({
+      user: req.user.id,
+      title: req.body.title || "Expense",
+      amount: -Math.abs(req.body.amount),
+      type: "expense",
+      category: req.body.category,
+    })
+
+    res.status(201).json(tx)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+export default router
