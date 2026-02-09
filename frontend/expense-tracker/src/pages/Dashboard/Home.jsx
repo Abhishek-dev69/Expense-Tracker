6b7e9559760eb01f300466d29dbcca1fbcf2021d
Abhishek <abhishek12439525@gmail.com>
2026-02-09 18:36:12 +0530

    UI changes

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { useAuth } from "../../context/AuthContext"
+import { BASE_URL } from "../../utils/apiPaths"
+
+import StatsGrid from "../../components/dashboard/StatsGrid"
+import TransactionsTable from "../../components/dashboard/TransactionsTable"
 
 const Home = () => {
+  const { token } = useAuth()
+  const authToken = token || localStorage.getItem("token")
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/api/dashboard`, {
+      headers: { Authorization: `Bearer ${authToken}` },
+    })
+      .then((res) => res.json())
+      .then(setData)
+  }, [])
+
+  if (!data) return null
+
   return (
-    <div>
-      <h1>Welcome to the Expense Tracker</h1>
-    </div>
+    <>
+      <StatsGrid data={data} />
+      <TransactionsTable transactions={data.transactions} />
+    </>
   )
 }
 
