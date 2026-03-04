423578009ebdc68bbfa0a51ce45763b13a9bcd60
Abhishek <abhishek12439525@gmail.com>
2026-03-04 22:43:42 +0530

    fixed transaction issue

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -7,23 +7,33 @@ const transactionSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
+
     title: {
       type: String,
       required: true,
     },
+
     amount: {
       type: Number,
       required: true,
     },
+
     type: {
       type: String,
       enum: ["income", "expense"],
       required: true,
     },
+
     category: {
       type: String,
       default: "General",
     },
+
+    aiConfidence: {
+      type: Number,
+      default: 0,
+    },
+
     date: {
       type: Date,
       default: Date.now,
